72130b15f23d6fafb883e1962f4f194fa5f50335
Ali Moos <mooosali446@gmail.com>
2025-10-29 05:36:54 +0000

    m

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"Second 1 letters: {word_second_1}")
+print(f"First 3 letters = {Hol}")
+print(f"Last 2 letters = {on}")
+print(f"Middle word = olberto")
+
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
