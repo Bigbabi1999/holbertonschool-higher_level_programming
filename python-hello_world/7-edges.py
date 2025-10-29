80e8a6f628d4100226da16f2fc9d4ce9340d6866
Ali Moos <mooosali446@gmail.com>
2025-10-29 05:37:54 +0000

    m

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -3,7 +3,6 @@ word = "Holberton"
 print(f"First 3 letters = {Hol}")
 print(f"Last 2 letters = {on}")
 print(f"Middle word = olberto")
-
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
