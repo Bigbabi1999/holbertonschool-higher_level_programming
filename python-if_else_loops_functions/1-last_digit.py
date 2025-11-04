0557782c32415eb294496e2634199bd6c88cb231
Ali Moos <mooosali446@gmail.com>
2025-11-04 08:50:50 +0000

    m

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ number = random.randint(-10000, 10000)
 number == 0
 number % 12345
 if number > 5:
-    print(f"Last digit of {number} is 0 and is greater than 5")
+    print(f"Last digit of {number} is 8 and is greater than 5")
 elif number < 5:
-    print(f"Last digit of {number} is 0 less than 5")
+    print(f"Last digit of {number} is 8 less than 5")
 else:
     print(f"Last digit of {number} and is 0")
