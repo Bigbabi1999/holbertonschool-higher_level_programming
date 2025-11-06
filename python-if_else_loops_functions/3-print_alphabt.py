388375e2912d117fdd8d84665a98f151263eda54
Ali Moos <mooosali446@gmail.com>
2025-11-06 14:09:49 +0000

    m

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,7 +2,8 @@
 
 
 for ascii in range(97, 123):
-    res = chr(ascii)
-if ascii == (113,101):
-    continue
-    print("{}".format(res), end="")
+    
+
+    if ascii != 101 and ascii != 113:
+    
+        print("{}".format(chr(ascii)), end="")
