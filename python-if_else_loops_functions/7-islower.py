d2a180b3925f93b6ca386842bb22726ecee5abe9
Ali Moos <mooosali446@gmail.com>
2025-11-10 10:35:41 +0000

    m

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def islower(c):
-    pass Traceback (most recent call last):
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
-islower(c)
+    x = ord(c)
+    if x >= 97 and x <= 122:
+        return True
+    else:
+        return False
+
