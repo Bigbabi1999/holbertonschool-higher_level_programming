37d4e58f934af9ba5b15f64e2c71abcc9b8dbd39
Ali Moos <mooosali446@gmail.com>
2025-11-10 11:04:16 +0000

    m

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def islower(c):
-    pass Traceback (most recent call last):
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
-islower()
+    x = ord(c)
+    if x >= 97 and x <= 122:
+        return False
