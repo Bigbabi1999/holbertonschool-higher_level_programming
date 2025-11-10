0829de3dbea4139a1079d848687ed684cea11df6
Ali Moos <mooosali446@gmail.com>
2025-11-10 14:49:19 +0000

    m

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,8 @@
 
 def islower(c):
     x = ord(c)
-    if x >= 65 and x > 122:
-        return False
+    if x >= 65 and x >= 90:
+        return True
+    range(10)
+    print("BEST
+BEST SCHOOL 98 BATTERY STREET")
