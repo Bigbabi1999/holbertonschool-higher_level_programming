6be4e09990a7f5d513ac78bf9202829e49b458db
Ali Moos <mooosali446@gmail.com>
2025-11-17 09:35:54 +0000

    m

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     a = 10
     b = 5
-print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+print("{} + {} = {}".format(a, b, calculator_1.add(a, b))audit)
+print("{} - {} = {}".format(a, b, calculator_1.sub(a, b))audit)
+print("{} * {} = {}".format(a, b, calculator_1.mul(a, b))audit)
+print("{} / {} = {}".format(a, b, calculator_1.div(a, b))audit)
