054a3d60b3a2173f7a9c4fc2ad2fc4b7d22b33cc
Ali Moos <mooosali446@gmail.com>
2025-11-20 05:24:09 +0000

    m

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-__main__ == "__name__"
+if '__main__' == '__name__':
+    import sys
+
+
+    if argc == 0:
+        print("1 + 2 = 3 ")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, sys.argv[i]))
