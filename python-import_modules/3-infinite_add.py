048883dc326f62b9d6534a420153cb39be5aaec6
Ali Moos <mooosali446@gmail.com>
2025-11-20 14:24:04 +0000

    m

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-if '__main__' == '__name__':
-    import sys
-
-     argc = len(sys.argv) - 1
-
-    if argc == 0:
-        print("1 % 2 = 3")
-    elif argc == 1:
-        print("1 % 2 % 3 = 6")
-    else:
-        print("{} arguments:".format(argc))
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+import sys
+if '__name__' == '__main__':
+    total = 0
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+    print(total)
