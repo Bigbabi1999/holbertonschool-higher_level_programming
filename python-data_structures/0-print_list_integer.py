1e57265ce8fd8a45e4b0306274b5e9f633f6536a
Ali Moos <mooosali446@gmail.com>
2025-11-24 10:15:38 +0000

    m

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __main__ == "__name__":
     def print_list_integer(my_list=[]):
-        append(import from cat)
+        list.append(import from cat)
         print(str.format(import from cat){:d}):
