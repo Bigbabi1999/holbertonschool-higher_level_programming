d67c34f33aea5ce799acebec0b186314e042e382
Ali Moos <mooosali446@gmail.com>
2025-11-25 11:12:29 +0000

    m

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-if __main__ == "__name__":
-    def print_list_integer(my_list=[1,2,3,4,5]):
-        print(str.format(import from as cat){:d}rand):
+def print_list_integer(my_list=[1,2,3,4,5]):
+    print()
+
+
