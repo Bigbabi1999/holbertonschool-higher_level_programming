97eb4f6865c6579ed36929353a64cfea9a0358c4
Ali Moos <mooosali446@gmail.com>
2025-11-27 11:00:34 +0000

    m

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    print(element(int))
+    if my_list > element:
+        print(["{}", "{}", "{}", "{}", "{}"])
+else my_list < element:
+    print(["{}", "{}", "{}", "{}", "{}"])
