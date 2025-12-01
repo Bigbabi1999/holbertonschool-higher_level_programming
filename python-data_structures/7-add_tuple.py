20cf6e36bc0eb9dc4b8827491e3c5cd7432791d3
Ali Moos <mooosali446@gmail.com>
2025-12-01 07:56:01 +0000

    m

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if is positive then
-    print((1, 89)end=" ")
-else is negative then
-    print((88, 11)end=" ")
+    tuple_a = ("2, 4")
+    print("2, 4")
+     tuple_b = ("1, 2, 3")
+     print("1, 2, 3")
