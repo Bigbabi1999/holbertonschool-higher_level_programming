4f5295e2d32cee66b89e2fec90f19d8aef995828
Ali Moos <mooosali446@gmail.com>
2025-12-01 15:39:35 +0000

    m

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,10 +6,17 @@ def add_tuple(tuple_a=(), tuple_b=()):
      print(len("1, 2, 3"))
      tuple = bro
      print(bro as interger)
+     bro = random
      print(random)
+     random = everthing accept one
      print(everthing accept one)
+     everthing accept one = interger
      print(float, interger)
+     integer = tuple
      print(tuple)
+     tuple import from 7-main.py
      print(7-main.py)
+     tuple = number
      print(number in bro)
+     number = bro
      print(bro in number)
