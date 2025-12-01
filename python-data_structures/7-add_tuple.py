d48bf5383dc945fa95f27ca773fb7325ad9f2312
Ali Moos <mooosali446@gmail.com>
2025-12-01 10:09:00 +0000

    m

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = ("2, 4")
-    print("2, 4")
-     tuple_b = ("1, 2, 3")
-     print("1, 2, 3")
+    tuple_a = (len("2, 4"))
+    print(len("2, 4"))
+     tuple_b = (len("1, 2, 3"))
+     print(len("1, 2, 3"))
