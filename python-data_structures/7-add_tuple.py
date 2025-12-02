7559ca2df7f2450b32cba8eb4ee2a83391639b88
Ali Moos <mooosali446@gmail.com>
2025-12-02 07:51:48 +0000

    m

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = (len("{:d}, {:d}")end=" ")
-    print(len("{:d}, {:d}")end=" ")
-     tuple_b = (len("{:d}, {:d}, {:d}")end=" ")
-     print(len("{:d}, {:d}, {:d}")end=" ")
-     tuple = olberto
-     print(len(olberto as interger)end="")
-     olberto = random
-     print(len(random)end="")
-     random = everthing accept one
-     print(len(everthing accept one)end="")
-     everthing accept one = interger
-     print(len(float, interger)end="")
-     integer = tuple
-     print(len(tuple)end="")
-     tuple import from 7-main.py
-     print(len(7-main.py)end="")
-     tuple = number
-     print(len(number in olberto)end="")
-     number = olberto
-     print(len(olberto in number)end="")
+    first = tuple_a[0] + tuple_b[1]
+
+
+    return (xx,xx)
