f6fe0a970d5caaf94da750dfcaa23a3b1b482128
Ali Moos <mooosali446@gmail.com>
2025-12-04 14:12:24 +0000

    m

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list > [0]:
-        print(0 divisible by "{:d}")
-else my_list < [0]
-    print("{:d}" divisible by 0)
+    new_list = []
 
-    return
+    for num in my_list:
+        if num % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
