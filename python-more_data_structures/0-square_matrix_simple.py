47f285620ad72436940bbd59823876c8995a5e5f
Ali Moos <mooosali446@gmail.com>
2025-12-08 06:43:38 +0000

    m

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     matrix = [[1, 4, 9], [16, 25, 36], [49, 64, 81]]
-    print([[1, 4, 9], [16, 25, 36], [49, 64, 81]] = new_matrix0)
-    matrix = new_matrix
+    print([[1, 4, 9], [16, 25, 36], [49, 64, 81]] == new_matrix0)
+    matrix == new_matrix
     return
