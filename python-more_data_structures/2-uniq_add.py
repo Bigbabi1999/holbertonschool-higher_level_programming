56d5bd93de7b6e92348c4cbdf995dbc2d3f11b9f
Ali Moos <mooosali446@gmail.com>
2025-12-10 06:45:37 +0000

    m

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    my_list = []
     if iter == my_list:
-        print(my_list)
+        my_list.append(my_list)
     else:
         iter != my_list
-        print(my_list)
+        my_list.append(my_list)
