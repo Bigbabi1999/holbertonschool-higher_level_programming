e8382f75efbb4c360384ad0701485288ba9ba295
Ali Moos <mooosali446@gmail.com>
2025-12-11 14:26:20 +0000

    m

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -6,7 +6,6 @@ def only_diff_elements(set_1, set_2):
         if item not in set_2:
             diff.add(item)
 
-
     for item in set_2:
         if item not in set_1:
             diff.add(item)
