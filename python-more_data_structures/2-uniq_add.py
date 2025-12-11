bded7b81432ace98d4263e9f14c916fb08b0fa7d
Ali Moos <mooosali446@gmail.com>
2025-12-11 07:25:45 +0000

    m

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    for ((1 + 2 + 3 + 1 + 4 + 2 + 5 == Result 15)True)
+    for (([1, 2, 3, 1, 4, 2, 5])my_list)
         my_list.append(my_list)
+        print(set(my_list))
         return
