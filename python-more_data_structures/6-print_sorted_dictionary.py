25fa3f9422dbe0840adc5d51609350ffc03a0415
Ali Moos <mooosali446@gmail.com>
2025-12-12 07:43:38 +0000

    m

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    dictionary != item:
+    if dictionary > item
+        print(dictionary)
+    else: dictionary < item
+        print(item)
