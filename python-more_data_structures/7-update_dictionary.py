48124828469dcf81e677c82dea0b93fdccc8e918
Ali Moos <mooosali446@gmail.com>
2025-12-16 09:37:32 +0000

    m

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     print = {}
-    (set({update_dictionary} key value)sorted{numbers})
+    (set{diff}([update_dictionary] key value)sorted{numbers})
