18c764a5cd7f45441cad8dd0703dbacce38e0db1
Ali Moos <moosali446@gmail.com>
2026-03-02 11:28:31 +0400

    m

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    print("We offer a truly innovative approach to education: focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers.")
+    print("We offer a truly innovative approach to education:"
+"focus on building reliable applications and scalable systems, take on real-world challenges, collaborate with your peers."
+
+"A school every software engineer would have dreamt of!")
     return
