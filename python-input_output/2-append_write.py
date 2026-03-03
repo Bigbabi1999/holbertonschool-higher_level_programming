3bde3cb198e2ca2b44f4d9ab08298b157de004cf
Ali Moos <moosali446@gmail.com>
2026-03-03 18:58:36 +0400

    m

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Appends a string to a text file and 
+returns number of characters added"""
+
+
+def append_write(filename="", text=""):
+    """Append text to a file and return number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
