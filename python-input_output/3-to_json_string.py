c22a601f35be5aa2ef3c17ba4ccc9062d0077f20
Ali Moos <moosali446@gmail.com>
2026-03-04 13:42:38 +0400

    m

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,5 +4,5 @@
 def to_json_string(my_obj):
     print(my_obj)
     """A++ = my_obj"""
-    """add Roblox Noob in my house and add stepsister and stepmother and cinderella in roblox house"""
+    """add Roblox Noob in my house and add cinderella and stepsister and stepmother and a father in Linux and Roblox House"""
     return
