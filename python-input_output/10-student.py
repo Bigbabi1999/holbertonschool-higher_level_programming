747ba996f724f1209cbee0dcac1849d049478d4d
Ali Moos <moosali446@gmail.com>
2026-03-10 19:03:26 +0400

    m

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""Student class"""
 
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary representation"""
+        if type(attrs) == list:
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+                return new_dict
+        return self.__dict__
