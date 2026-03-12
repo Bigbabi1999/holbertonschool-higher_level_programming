e0aef9aedde75469d6832c6daee0c4776c76c9fc
Ali Moos <moosali446@gmail.com>
2026-03-12 18:50:34 +0400

    m

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,7 +13,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """Return dictionary representation"""
-        if type(attrs) == list:
+        if isinstance(attrs, list):
             new_dict = {}
             for key in attrs:
                 if key in self.__dict__:
