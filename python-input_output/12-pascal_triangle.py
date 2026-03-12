4bdec2914e3f8f7c761a963348538b7000560634
Ali Moos <moosali446@gmail.com>
2026-03-12 19:15:05 +0400

    m

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""pascal's Triangle"""
 
+
+def pascal_triangle(n):
+    """Return Pascal's triangle of n"""
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+
+        if triangle:
+            last_row = triangle[-1]
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j + 1])
+            row.append(1)
+
+        triangle.append(row)
+
+    return triangle
