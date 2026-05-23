4bc177eff7ac23252cffe1e19757c0841c06ed46
Ali Moos <moosali446@gmail.com>
2026-05-23 18:09:39 +0400

    m

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for adding two integers"""
+
+def add_integer(a, b=98):
+    """Adds two integers or floats"""
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    a = int(a)
+    b = int(b)
+
+    return a + b
