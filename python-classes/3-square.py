0a5ee69b3b18e1128b803b50ae9d54ca0ce2abad
Ali Moos <moosali446@gmail.com>
2026-06-30 18:27:40 +0400

    m

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,7 +14,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         
-        self.__size = size
+        self.size = size
 
     def area(self):
         """Return the current square area"""
