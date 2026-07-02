f852240b3bb57b222b5ed15c99a80b885ef93d95
Ali Moos <moosali446@gmail.com>
2026-07-02 18:22:51 +0400

    m

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,9 +19,9 @@ class Square:
     def size(self, value):
         """Set the size with validation"""
         if not isinstance(value, int):
-            raise TypeError("size must be an intger")
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
