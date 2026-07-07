7496ae0918ff3e1d6a37976500ef890a75a8a155
Ali Moos <moosali446@gmail.com>
2026-07-07 18:31:16 +0400

    m

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Defines a rectangle class"""
+
+
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width (self):
+        """Retrieve the width"""
+        return self.__width
