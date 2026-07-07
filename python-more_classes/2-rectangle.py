437c55ab0a0e9c957f31fc253b78209aae893413
Ali Moos <moosali446@gmail.com>
2026-07-07 18:25:42 +0400

    m

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3# 
+#!/usr/bin/python3#
 """Defines a Rectangle class"""
 
 
@@ -41,7 +41,7 @@ class Rectangle:
     def area(self):
         """Return the area of the rectangle"""
         return self.__width * self.__height
-    
+
     def perimeter(self):
         """Return the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
