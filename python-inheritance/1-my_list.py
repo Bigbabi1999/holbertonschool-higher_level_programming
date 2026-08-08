09b7f4b0652ed11dc0c1c320fc7a3099f7af36c9
Ali Moos <moosali446@gmail.com>
2026-08-08 18:26:37 +0400

    m

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a MyList class"""
+
+
+class MyList(list):
+    """A list with a method to print its sorted version"""
+
+
+def print_sorted(self):
+    """print the list sorted in ascending order"""
+    print(sorted(self))
