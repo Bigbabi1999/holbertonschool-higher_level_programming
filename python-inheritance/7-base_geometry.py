07df27d75eea74490b23359fd68cc09f6e9ce4d9
Ali Moos <moosali446@gmail.com>
2026-08-18 18:15:40 +0400

    m

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Defines the BaseGemoetry class"""
+
+
+class BaseGeometry:
+    """Represents the base geometry"""
+
+    def area(self):
+        """Raise an exception because area is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer"""
+        if type(name) is not str:
+            raise TypeError("name must be a string")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater then 0".format(name))
