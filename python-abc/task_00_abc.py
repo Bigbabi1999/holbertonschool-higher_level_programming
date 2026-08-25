164387696d5895bada4e625a546931360caed2f7
Ali Moos <moosali446@gmail.com>
2026-08-25 18:18:59 +0400

    m

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+"""Defines an abstract Animal class and its subclass"""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract base class representing an animal"""
+
+    @abstractmethod
+    def sound(self):
+        """Return the sound made by the animal"""
+        pass
+
+
+class Dog(Animal):
+    """Represents a dog"""
+
+    def sound(self):
+        """Return the sound made by a dog"""
+        return "Bark"
+
+
+class Cat(Animal):
+    """Represents a cat"""
+
+    def sound(self):
+        """Return the sound made by a cat"""
+        return "Meow"
