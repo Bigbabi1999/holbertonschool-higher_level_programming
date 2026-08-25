64679329fe37ce7ba096bfc18699193937f3987f
Ali Moos <moosali446@gmail.com>
2026-08-25 18:32:29 +0400

    m

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+"""Defines an absract Shape class and its subclass"""
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """Abstract base class for shapes"""
+
+    @abstractmethod
+    def area(self):
+        """Calculate the area of the shape"""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Calculate the perimeter of the shape"""
+        pass
+
+
+class Circle(Shape):
+    """Represents a circle"""
+
+    def __init__(self, radius):
+        """Initialize a circle with a radius"""
+        self.radius = radius
+
+    def area(self):
+        """Calculate the area of the circle"""
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        """Calculate the perimeter of the circle"""
+        return 2 * math.pi * self.radius
