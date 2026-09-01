965561a09f6f43cade308cb36f2e752f0c33bccf
Ali Moos <moosali446@gmail.com>
2026-09-01 18:34:36 +0400

    m

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Defines Fish, Bird, and FlyingFish classs"""
+
+
+class Fish:
+    """Represents a fish"""
+
+    def swim(self):
+        """Print that the fish is swimming"""
+        print("The fish is swimming")
+
+        def habitat(self):
+            """Print the fish's habitat"""
+            print("The fish lives in water")
+
+
+class Bird:
+    """Represents a bird"""
+
+    def fly(self):
+        """Prince that the bird is fliying"""
+        print("The bird is flying")
+        print("The bird lives in the sky")
