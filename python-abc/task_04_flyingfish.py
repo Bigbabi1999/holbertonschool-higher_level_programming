f015fc36e1784d16d97cac4c61c55473f808f387
Ali Moos <moosali446@gmail.com>
2026-09-03 18:12:21 +0400

    m

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -20,4 +20,23 @@ class Bird:
     def fly(self):
         """Prince that the bird is fliying"""
         print("The bird is flying")
+
+    def habitat(self):
+        """Print the bird's habitat"""
         print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Represents a flying fish"""
+
+    def fly(self):
+        """Print that the flying fish is soaring"""
+        print("The flying fish is saoring")
+
+    def swim(self):
+        """Print that the flying fish is swimming"""
+        print("The flying fish is swimming")
+
+    def habitat(self):
+        """Print the flying fish's habitat"""
+        print("The flying fish lives both in the water and the sky")
